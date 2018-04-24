ea3f7f6771ad5ad1d43160650cecf3c97aacb7b5
Anna Moser <S1610238059@students.fh-hagenberg.at>
2018-04-24 12:59:04 +0200

    draganddrop

---
+++ src/objects/Plant.js
@@ -0,0 +1,22 @@
+/**
+ * Created by alinaisabelle on 23.04.18.
+ */
+
+class Plant extends Phaser.Sprite {
+
+    constructor(game, x, y, name) {
+
+        super(game, x, y, 'plant');
+
+        this.anchor.x = 0.5;
+        this.anchor.y = 0.5;
+
+        this.scale.x = 0.1;
+        this.scale.y = 0.1;
+
+        this.game.stage.addChild(this);
+    }
+
+}
+
+export default Plant;
