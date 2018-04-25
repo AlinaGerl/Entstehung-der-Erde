f057412693e5441da66cdb5ddee90d23988be5d1
Alina Gerl <alina.gerl@hotmail.com>
2018-04-25 12:41:06 +0200

    PlantsFinished

--- src/states/DragndropPlants.js
+++ src/states/DragndropPlants.js
@@ -3,33 +3,170 @@
  */
 import Earth from 'objects/Earth';
 import Plant from 'objects/Plant';
+import Translation from 'utils/translate';
+import Text from 'objects/text';
 
 class DragndropPlants extends Phaser.State {
 
     create() {
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        let center = { x: this.game.world.centerX, y: this.game.world.centerY };
-        this.earth = new Earth(this.game, center.x, center.y, 'firstEarth', this.game.earthRotate);
-        console.info(this.game.earthRotate);
+        this.earth = new Earth(this.game, this.game.world.centerX, this.game.world.centerY, 'firstEarth', this.game.earthRotate);
 
-        this.plant = new Plant(this.game, 70, 70, 'plant');
+        //text
+        this.translation = new Translation(this.game);
+        let text = this.translation.translate("first10_1");
+        this.textbox = new Text(this.game, text);
 
-        this.game.physics.enable( [ this.earth, this.plant ], Phaser.Physics.ARCADE);
 
-        this.plant.inputEnabled = true;
-        this.plant.input.enableDrag(true);
+        this.animals = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'oceanAnimals');
+        this.animals.scale.x = 0; this.animals.scale.y = 0; this.animals.anchor.y = 0.5; this.animals.anchor.x = 0.5;
+        this.firstStage = true;
+        this.secondStage = false;
+
+
+        //enable mouse input
+        this.game.input.mouse.capture = true;
+
+        //counts plants on earth
+        this.plantsCount = 0;
+
+        //z-depth
+        let earthG = this.game.add.group();
+        let animalsG = this.game.add.group();
+        earthG.add(this.earth);
+        animalsG.add(this.animals);
+        earthG.z = 100
+        animalsG.z = 120;
+        // this.plant = new Plant(this.game, 70, 70, 'plant');
+        //
+        this.game.physics.enable( [ this.earth], Phaser.Physics.ARCADE);
+        //
+        // this.plant.inputEnabled = true;
+        // this.plant.input.enableDrag(true);
     }
 
     update(){
-        this.game.physics.arcade.collide(this.earth, this.plant, this.collisionHandler, null, this);
+        if (this.firstStage && this.game.input.activePointer.leftButton.isDown) {
+            this.firstStage = false;
+            this.getIntoWater();
+        }
+        if (this.secondStage && this.game.input.activePointer.leftButton.isDown) {
+            this.secondStage = false;
+            this.outOfWater();
+        }
+
+        if (this.plantsCount === 6) {
+            //this.items.anchor.x = 0.5; this.items.anchor.y = 0.5;
+            this.plantsRotate = true;
+            this.lastText();
+        }
+        if (this.plantsRotate) this.items.angle -= 0.03;
+        //this.game.physics.arcade.collide(this.earth, this.plant, this.collisionHandler, null, this);
+    }
+
+    getIntoWater() {
+        this.textbox.destroy();
+        this.game.add.tween(this.earth.scale).to({ x: 5, y: 5}, 3000, Phaser.Easing.Cubic.InOut, true, );
+        this.game.add.tween(this.animals.scale).to({ x: 1, y: 1}, 3000, Phaser.Easing.Cubic.InOut, true, );
+        this.game.time.events.add(Phaser.Timer.SECOND * 3, this.firstText, this);
+
+    }
+
+    outOfWater() {
+        this.textbox.destroy();
+        this.game.add.tween(this.earth.scale).to({ x: 0.5, y: 0.5}, 3000, Phaser.Easing.Cubic.InOut, true, );
+        this.game.add.tween(this.animals.scale).to({ x: 0, y: 0}, 3000, Phaser.Easing.Cubic.InOut, true, );
+        this.game.time.events.add(Phaser.Timer.SECOND * 3, this.secondText, this);
+
+    }
+
+    firstText() {
+        let text = this.translation.translate("first10_2");
+        this.textbox = new Text(this.game, text);
+        this.secondStage = true;
+
+    }
+
+    secondText() {
+        let text = this.translation.translate("first10_3");
+        this.textbox = new Text(this.game, text);
+        this.animals.destroy();
+
+        this.items = this.game.add.group();
+        this.items.x = this.game.world.centerX;
+        this.items.y = this.game.world.centerY;
+
+        let item;
+        for (var i = 0; i < 6; i++)
+        {
+            // Directly create sprites from the group.
+            item = this.items.create((-this.game.world.centerX+90), (-this.game.world.centerY + 100 + 90 * i), 'plant', i);
+            item.anchor.x = 0.5;
+            item.anchor.y = 0.5;
+            item.scale.x = 0.03;
+            item.scale.y = 0.03;
+            item.name = 'block' + i;
+
+            // Enable input detection, then it's possible be dragged.
+            item.inputEnabled = true;
+
+            // Make this item draggable.
+            item.input.enableDrag();
+
+            // Then we make it snap to 90x90 grids.
+            //item.input.enableSnap(50, 50, false, true);
+
+            // Add a handler to remove it using different options when dropped.
+            this.game.physics.enable( [ item ], Phaser.Physics.ARCADE);
+            item.events.onDragStop.add(this.dropHandler, this);
+        }
+
+
+
+    }
+
+    lastText() {
+        this.textbox.destroy();
+        let text = this.translation.translate("last10");
+        this.textbox = new Text(this.game, text);
+    }
+
+    dropHandler(item, pointer) {
+
+        // if (item.x < 90) {
+        //     item.x = 90;
+        // }
+        // else if (item.x > 400) {
+        //     //  Remove the item from the Group.
+        //     this.items.remove(item);
+        // }
+
+        this.isWrong = false;
+        this.game.physics.arcade.collide(this.items.children, item, this.stopCollision, null, this);
+        this.game.physics.arcade.collide(this.earth, item, this.collisionHandler, null, this);
+
+    }
+
+    stopCollision (obj1, obj2) {
+        this.isWrong = true;
+        //this.plantsCount--;
+        obj2.x = 500;
+        obj2.inputEnabled = true;
+        obj2.input.enableDrag();
+
     }
 
     collisionHandler (obj1, obj2) {
-        //  The two sprites are colliding
-        this.game.stage.backgroundColor = '#992d2d';
+        if (!this.isWrong) {
+            this.plantsCount++;
+            obj2.inputEnabled = false;
+            //this.game.stage.backgroundColor = '#992d2d';
+        }
 
     }
+
+
 
 }
 
